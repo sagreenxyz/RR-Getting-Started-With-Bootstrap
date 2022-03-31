bc64dc5fadbb4e73776c7e1cda114423e51653aa
Scott Green <sagreenxyz@gmail.com>
2022-03-30 20:39:51 -0500

    3. adding a customized Bootstrap Button component to App component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
-
+import Button from 'react-bootstrap/Button'
 
 
 function App() {
   return (
     <div className="App">
       <h1>Bootstrap Website</h1>
-     
+     <Button>
+       Next
+     </Button>
     </div>
   );
 }
