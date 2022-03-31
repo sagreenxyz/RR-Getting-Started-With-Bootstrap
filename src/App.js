145aa28eafc5f938c75f18f93c0a12adadd376b1
Scott Green <sagreenxyz@gmail.com>
2022-03-30 20:46:03 -0500

    6. modifying the Nav component to fill the horizontal space and show as tabs variant

--- src/App.js
+++ src/App.js
@@ -5,12 +5,24 @@ import Nav from 'react-bootstrap/Nav'
 function App() {
   return (
     <div className="App">
-      <Nav fill activeKey="/home">
+      <Nav fill variant="tabs" activeKey="/home">
         <Nav.Item>
           <Nav.Link href="/home">Home</Nav.Link>
         </Nav.Item>
         <Nav.Item>
           <Nav.Link eventKey="/about">About Us</Nav.Link>
+        </Nav.Item>
+        <Nav.Item>
+          <Nav.Link eventKey="/test1">Test 1</Nav.Link>
+        </Nav.Item>
+        <Nav.Item>
+          <Nav.Link eventKey="/test2">Test 2</Nav.Link>
+        </Nav.Item>
+        <Nav.Item>
+          <Nav.Link eventKey="/test3">Test 3</Nav.Link>
+        </Nav.Item>
+        <Nav.Item>
+          <Nav.Link eventKey="/atest4bout">Test 4</Nav.Link>
         </Nav.Item>
       </Nav>
 
