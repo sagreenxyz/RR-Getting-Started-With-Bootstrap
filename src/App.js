191b4c9db5b98be6e5b3e1f29d5b314f0b654128
Scott Green <sagreenxyz@gmail.com>
2022-03-30 20:51:24 -0500

    7. adding Bootstrap Form component; notice Context.Providers are added to Nav and Form

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import './App.css';
 import Button from 'react-bootstrap/Button'
 import Nav from 'react-bootstrap/Nav'
+import Form from 'react-bootstrap/Form'
 
 function App() {
   return (
@@ -27,9 +28,34 @@ function App() {
       </Nav>
 
       <h1>Bootstrap Website</h1>
-      <Button>
-        Next
-      </Button>
+      <div>
+        <Button>
+          Next
+        </Button>
+      </div>
+
+      <Form>
+        <Form.Group controlId="formBasicEmail">
+          <Form.Label>Email address</Form.Label>
+          <Form.Control type="email" placeholder="Enter email" />
+          <Form.Text className="text-muted">
+            We'll never share your email with anyone else.
+          </Form.Text>
+        </Form.Group>
+
+        <Form.Group controlId="formBasicPassword">
+          <Form.Label>Password</Form.Label>
+          <Form.Control type="password" placeholder="Password" />
+        </Form.Group>
+
+        <Form.Group controlId="formBasicCheckbox">
+          <Form.Check type="checkbox" label="Check me out" />
+        </Form.Group>
+
+        <Button variant="primary" type="submit">
+          Submit
+        </Button>
+      </Form>
     </div>
   );
 }
