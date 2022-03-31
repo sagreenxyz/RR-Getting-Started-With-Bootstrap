d55d676a6e6aedebbd606664e1c92f1220728bbf
Scott Green <sagreenxyz@gmail.com>
2022-03-30 20:41:38 -0500

    4. adding Nav to the top of the App component

--- src/App.js
+++ src/App.js
@@ -1,14 +1,23 @@
 import './App.css';
 import Button from 'react-bootstrap/Button'
-
+import Nav from 'react-bootstrap/Nav'
 
 function App() {
   return (
     <div className="App">
+      <Nav activeKey="/home">
+        <Nav.Item>
+          <Nav.Link href="/home">Home</Nav.Link>
+        </Nav.Item>
+        <Nav.Item>
+          <Nav.Link eventKey="/about">About Us</Nav.Link>
+        </Nav.Item>
+      </Nav>
+
       <h1>Bootstrap Website</h1>
-     <Button>
-       Next
-     </Button>
+      <Button>
+        Next
+      </Button>
     </div>
   );
 }
