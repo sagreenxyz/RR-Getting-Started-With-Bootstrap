e7dbeea97da3d65fac57fa623abee3b077fe9f7b
Scott Green <sagreenxyz@gmail.com>
2022-03-30 20:58:16 -0500

    10. adding a Table component

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Button from 'react-bootstrap/Button'
 import Nav from 'react-bootstrap/Nav'
 import Form from 'react-bootstrap/Form'
 import Card from 'react-bootstrap/Card'
+import Table from 'react-bootstrap/Table'
 
 function App() {
   return (
@@ -41,6 +42,36 @@ function App() {
           <Button variant="primary">Go somewhere</Button>
         </Card.Body>
       </Card>
+
+      <Table striped bordered hover>
+        <thead>
+          <tr>
+            <th>#</th>
+            <th>First Name</th>
+            <th>Last Name</th>
+            <th>Username</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>1</td>
+            <td>Mark</td>
+            <td>Otto</td>
+            <td>@mdo</td>
+          </tr>
+          <tr>
+            <td>2</td>
+            <td>Jacob</td>
+            <td>Thornton</td>
+            <td>@fat</td>
+          </tr>
+          <tr>
+            <td>3</td>
+            <td colSpan={2}>Larry the Bird</td>
+            <td>@twitter</td>
+          </tr>
+        </tbody>
+      </Table>
 
 
       <div>
